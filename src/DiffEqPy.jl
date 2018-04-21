bda313143f8b78db6e0dc0943405f980cd68a9a4
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-04-21 11:25:40 -0700

    initial package setup

--- src/DiffEqPy.jl
+++ src/DiffEqPy.jl
@@ -1,5 +1,16 @@
 module DiffEqPy
 
-# package code goes here
+using Reexport
+@reexport using DifferentialEquations
+using PyCall
+pysolve = PyCall.pyfunctionret(solve,Any,PyCall.PyAny)
+
+export pysolve
+
+@pyimport inspect
+function DiffEqBase.numargs(f::PyCall.PyObject)
+    haskey(f,:py_func) ? _f = f[:py_func] : _f = f
+    length(first(inspect.getfullargspec(_f)))
+end
 
 end # module
