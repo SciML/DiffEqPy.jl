050a088bb2a69355800503b717c8945c24d05089
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-04-26 13:18:45 -0700

    fix length calc

--- src/DiffEqPy.jl
+++ src/DiffEqPy.jl
@@ -5,15 +5,19 @@ using Reexport
 using PyCall
 pysolve = PyCall.pyfunctionret(solve,Any,Vararg{PyCall.PyAny})
 
+const inspect = PyNULL()
+function __init__()
+    copy!(inspect, pyimport("inspect"))
+end
+
 export pysolve
 
-@pyimport inspect
 function DiffEqBase.numargs(f::PyCall.PyObject)
     haskey(f,:py_func) ? _f = f[:py_func] : _f = f
     if PyCall.pyversion < v"3.0.0"
-        return length(first(inspect.getargspec(_f)))
+        return length(first(inspect[:getargspec](_f)))
     else
-        return length(first(inspect.getfullargspec(_f)))
+        return length(first(inspect[:getfullargspec](_f)))
     end
 end
 
